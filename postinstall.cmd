11d716f466a8876583e374dd56b92ee82f89a4ad
Marnix0810 <48680596+Marnix0810@users.noreply.github.com>
2019-09-07 13:45:10 +0200

    Delete postinstall.cmd

--- postinstall.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-title preparing muZikDL for first usage.
-cls
-echo installing Microsoft Visual C++ 2010 Redistributable Package...
-start /wait "" "%~dp0bin\vcredist_x86.exe" /install /quiet
-:d
-cls
-echo All done!
