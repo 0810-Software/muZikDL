b172375130c5e40d85194a509aa5e56691ffe478
Marnix0810 <48680596+Marnix0810@users.noreply.github.com>
2019-09-20 23:10:26 +0200

    Update MuZikDL_install_script.cmd

--- MuZikDL_install_script.cmd
+++ MuZikDL_install_script.cmd
@@ -24,28 +24,32 @@ start /wait "" "7z1900-extra.exe" -o"%randtmp%" -y && echo 7za is ready to use
 cls
 echo Unpacking MuZikDL files...
 set appdir=C:\Users\marni\AppData\Local\Programs\Marnix0810\muZikDL
-7za X MuZikDL.zip -o"%appdir%" -y
+set appdirp=C:\Users\marni\AppData\Local\Programs\Marnix0810
+7za X MuZikDL.zip -o"%appdirp%" -y
+ren "%appdirp%\muZikDL-master" muZikDL
 
 cls
 echo installing Microsoft Visual C++ 2010 Redistributable Package...
-start /wait "" "%appdir%\muZikDL-master\bin\Installer-only\vcredist_x86.exe" /install /quiet
+start /wait "" "%appdir%\bin\Installer-only\vcredist_x86.exe" /install /quiet
 
 cls
 echo Creating shortcuts...
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HOMEDRIVE%\Users\Public\Desktop\MuZikDL.lnk');$s.TargetPath='%appdir%\muZikDL-master\MuZikDL.cmd';$s.Save()"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\MuZikDL.lnk');$s.TargetPath='%appdir%\muZikDL-master\MuZikDL.cmd';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HOMEDRIVE%\Users\Public\Desktop\MuZikDL.lnk');$s.TargetPath='%appdir%\MuZikDL.cmd';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\MuZikDL.lnk');$s.TargetPath='%appdir%\MuZikDL.cmd';$s.Save()"
 setx path "%PATH%;%appdir%"
 
 cls
 echo Cleaning up installer files...
 del /q /s "%randtmp%*"
-del /q /s "%appdir%\muZikDL-master\bin\Installer-only\*"
+del /q /s "%appdir%\bin\Installer-only\*"
 cd "%~dp0"
-rd /s /q "%appdir%\muZikDL-master\bin\Installer-only\"
+rd /s /q "%appdir%\bin\Installer-only\"
 rd /s /q "%randtmp%"
+del /q /s "%appdir%\%~nx0"
+
 
 cls
 Echo the installation of MuZikDL is finished.
-start /min "" "https://github.com/Marnix0810/muZikDL/blob/master/LICENSE"
-start cmd /c "%appdir%\muZikDL-master\MuZikDL.cmd"
+start /min "" "https://marnix0810.github.io/muZikDL/License"
+start cmd /c "%appdir%\MuZikDL.cmd"
 exit
