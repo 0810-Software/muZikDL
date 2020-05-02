49c8e3c07b38fcdc1ab5b7a1e8bd9b662fd3d886
Marnix B <programmer.marxin0810@gmail.com>
2020-05-02 18:06:17 +0200

    Update MuZikDL.cmd

--- MuZikDL.cmd
+++ MuZikDL.cmd
@@ -17,14 +17,18 @@ echo no files in download qeue.
 start cmd /c "%~dp0Scripts\add_music_to_the_list.cmd"
 goto bye
 )
+:dl
 echo list of downloads:
 type downloadlist.txt
 cls
 call "%~dp0bin\youtube-dl.exe" --batch-file downloadlist.txt --extract-audio --audio-format mp3 -k || (
+call "%~dp0bin\youtube-dl.exe" --rm-cache-dir
 cls
-echo there has been an error downloading this list. You can try to add the items to the list and download again.
+echo there has been an error downloading this list.
+echo Press a key to retry or close to stop trying.
+echo If you wait for some time, it might help.
 pause
-exit
+goto dl
 )
 del "%tmp%\Marnix0810\MuZikDL\output\*" /S /Q
 rd /S /Q "%tmp%\Marnix0810\MuZikDL\output\"
